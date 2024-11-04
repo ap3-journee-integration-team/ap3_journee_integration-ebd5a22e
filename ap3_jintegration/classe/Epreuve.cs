e0b1f216c4c14659ca023a07240faa725d3677d7
lesliefauchon <leslie.milasido@gmail.com>
2024-11-04 10:42:07 +0100

    création classe epreuve

--- ap3_jintegration/classe/Epreuve.cs
+++ ap3_jintegration/classe/Epreuve.cs
@@ -2,6 +2,22 @@
 {
     public class Epreuve
     {
+        //nom coeff desc 
+        private string nom;
+        private int coeff;
+        private string description;
+
+        public void epreuve(string Nom, int Coeff, string Description)
+        {
+            this.nom = Nom;
+            this.coeff = Coeff;
+            this.description = Description;
+        }
+
+        public string Nom { get => nom; set => nom = value; }
+        public int Coeff { get => coeff; set => coeff = value; }
+        public string Description { get => description; set => description = value; }
+
 
     }
 }
