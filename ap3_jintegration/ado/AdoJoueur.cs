21ca3b2869d67f8f5f6e2e502d8d812c654f6d49
celinefedila <root@gmail.com>
2024-11-04 11:38:42 +0100

    création Ado, AdoEquipe et AdoJoueur

---
+++ ap3_jintegration/ado/AdoJoueur.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ap3_jintegration.ado
+{
+    internal class AdoJoueur : Ado
+    {
+        public static void getAll()
+        {
+            open();
+            string query = $"SELECT * FROM Joueur";
+            SqlCommand cmd = new SqlCommand(query, connexion);
+            cmd.ExecuteNonQuery();
+            close();
+        }
+    }
+}
