c8dbeebe4b7c82f34337ab8d69df7d5a27a48aa5
celinefedila <root@gmail.com>
2024-11-24 19:01:41 +0100

    remise à zéro classes ado, AdoEquipe, adoJoueur, Equipe, Joueur, Tournois et Epreuve créées

---
+++ ap3_jintegration/ado/adoJoueur.cs
@@ -0,0 +1,24 @@
+﻿using ap3_jintegration.classe;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ap3_jintegration.ado
+{
+    internal class adoJoueur : Ado 
+    {
+        public static void getAll()
+        {
+            open();
+            //string query = $"SELECT * FROM Joueur";
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connexion;
+            cmd.CommandText = "SELECT * FROM joueur";
+            cmd.ExecuteNonQuery();
+            close();
+        }
+    }
+}
---
+++ ap3_jintegration/classe/Equipe.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ap3_jintegration.classe
+{
+    public class Equipe
+    {
+        private string nom_equipe;
+        private List<Joueur> joueur;
+        private Dictionary<Epreuve, int> resultat; //création d'un dictionnaire vide avec juste des contraintes de forme : attend un type Epreuve et un type int
+        // ajouter methode .add dans la classe pas la bdd
+
+        public Equipe()
+        {
+        }
+
+        public Equipe(string nom_equipe)
+        {
+            this.nom_equipe = nom_equipe;
+            this.joueur = new List<Joueur>();
+            this.resultat = new Dictionary<Epreuve, int>();
+        }
+
+        public string Nom_equipe { get => nom_equipe; set => nom_equipe = value; }
+        public List<Joueur> Joueur { get => joueur; set => joueur = value; }
+        internal Dictionary<Epreuve, int> Resultat { get => resultat; set => resultat = value; }
+
+        //public void inscrireEp (Epreuve ep)
+        //{
+        //    if (resultat.ContainsKey(ep))
+        //    {
+
+        //    }
+        //}
+
+
+        //public void addScore(Epreuve ep, int scoreEp)
+        //{
+        //    if (resultat.ContainsKey(ep))
+        //    { 
+        //        for ()
+        //    }
+        //}
+    }
+}
